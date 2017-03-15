130373eff5078e412e7659c6d7d07cddf2ca3255
tiagoweb <tiagoweb@gmail.com>
2017-03-15 18:51:13 -0400

    grupo 7 - exercício 20

---
+++ lista1.5/exercicio20.py
@@ -0,0 +1,13 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Edson de Lima Barros                      1715310043
+# Gabriel Nascimento de Oliveira            1715310052
+# Luiz Daniel Raposo Nunes de Mello	        1715310049
+# Tiago Ferreira Aranha 	                1715310047
+# Wilbert Luís Evangelista Marins           1715310055
+# Mackson Garcez Moreno de Oliveira júnior  1215090300
+#
+# 20.
+# ----------------------------------------------------------
